5d539acbd918c2a96f1e42c3c10112a386d7552c
Kirti <kirtiguptakirti623@gmail.com>
2024-08-23 23:30:31 +0530

    Iteration 3 Loops done

--- js/index.js
+++ js/index.js
@@ -13,6 +13,7 @@ console.log("The navigator's name is " + hacker2);
 
 // Iteration 2: Conditionals
 // 2.1 Compare the lengths of the names and print the appropriate message
+
 if (hacker1.length > hacker2.length) {
     console.log("The driver has the longest name, it has " + hacker1.length + " characters.");
   } else if (hacker2.length > hacker1.length) {
@@ -22,3 +23,25 @@ if (hacker1.length > hacker2.length) {
   }
 
 // Iteration 3: Loops
+// 3.1 Print all the characters of the driver's name, separated by a space and in capitals
+let driverNameWithSpaces = "";
+for (let i = 0; i < hacker1.length; i++) {
+  driverNameWithSpaces += hacker1[i].toUpperCase() + " ";
+}
+console.log(driverNameWithSpaces.trim());
+
+// 3.2 Print all the characters of the navigator's name in reverse order
+let navigatorNameReversed = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  navigatorNameReversed += hacker2[i];
+}
+console.log(navigatorNameReversed);
+
+// 3.3 Lexicographic order comparison
+if (hacker1 < hacker2) {
+  console.log("The driver's name goes first.");
+} else if (hacker1 > hacker2) {
+  console.log("Yo, the navigator goes first definitely.");
+} else {
+  console.log("What?! You both have the same name?");
+}

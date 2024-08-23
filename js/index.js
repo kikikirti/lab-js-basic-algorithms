32a45aaeee9a6919f9d897266b8f46eddd01d990
Kirti <kirtiguptakirti623@gmail.com>
2024-08-23 23:19:19 +0530

    Iteration 1 names and input done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 console.log('I\'m ready!');
+
 // Iteration 1: Names and Input
-//
+
+// 1.1 Create a variable hacker1 with the driver's name
+// 1.2 Print "The driver's name is XXXX"
+// 1.3 Create a variable hacker2 with the navigator's name
+// 1.4 Print "The navigator's name is "YYYY"
+let hacker1 = "Kirti";
+console.log("The driver's name is " + hacker1);
+let hacker2 = "Lisa";
+console.log("The navigator's name is " + hacker2);
+
 // Iteration 2: Conditionals
 
 
